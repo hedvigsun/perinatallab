c29262cf1993eae0b7f11f66db46ce32c3bcc458
Hedvig <60605880+hedvigsun@users.noreply.github.com>
2024-04-24 16:56:53 +0200

    csa

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -1,78 +1,3 @@
-// /*
-// * Greedy Navigation
-// *
-// * http://codepen.io/lukejacksonn/pen/PwmwWV
-// *
-// */
-
-// var $nav = $('#site-nav');
-// var $btn = $('#site-nav button');
-// var $vlinks = $('#site-nav .visible-links');
-// var $hlinks = $('#site-nav .hidden-links');
-
-// var breaks = [];
-
-// function updateNav() {
-
-//   var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
-
-//   // The visible list is overflowing the nav
-//   if($vlinks.width() > availableSpace) {
-
-//     // Record the width of the list
-//     breaks.push($vlinks.width());
-
-//     // Move item to the hidden list
-//     $vlinks.children().last().prependTo($hlinks);
-
-//     // Show the dropdown btn
-//     if($btn.hasClass('hidden')) {
-//       $btn.removeClass('hidden');
-//     }
-
-//   // The visible list is not overflowing
-//   } else {
-
-//     // There is space for another item in the nav
-//     if(availableSpace > breaks[breaks.length-1]) {
-
-//       // Move the item to the visible list
-//       $hlinks.children().first().appendTo($vlinks);
-//       breaks.pop();
-//     }
-
-//     // Hide the dropdown btn if hidden list is empty
-//     if(breaks.length < 1) {
-//       $btn.addClass('hidden');
-//       $hlinks.addClass('hidden');
-//     }
-//   }
-
-//   // Keep counter updated
-//   $btn.attr("count", breaks.length);
-
-//   // Recur if the visible list is still overflowing the nav
-//   if($vlinks.width() > availableSpace) {
-//     updateNav();
-//   }
-
-// }
-
-// // Window listeners
-
-// $(window).resize(function() {
-//   updateNav();
-// });
-
-// $btn.on('click', function() {
-//   $hlinks.toggleClass('hidden');
-//   $(this).toggleClass('close');
-// });
-
-// updateNav();
-
-
-
 
 /*
 GreedyNav.js - https://github.com/lukejacksonn/GreedyNav
@@ -201,3 +126,81 @@ $(function() {
   } else check();
 
 });
+
+
+
+// /*
+// * Greedy Navigation
+// *
+// * http://codepen.io/lukejacksonn/pen/PwmwWV
+// *
+// */
+
+// var $nav = $('#site-nav');
+// var $btn = $('#site-nav button');
+// var $vlinks = $('#site-nav .visible-links');
+// var $hlinks = $('#site-nav .hidden-links');
+
+// var breaks = [];
+
+// function updateNav() {
+
+//   var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
+
+//   // The visible list is overflowing the nav
+//   if($vlinks.width() > availableSpace) {
+
+//     // Record the width of the list
+//     breaks.push($vlinks.width());
+
+//     // Move item to the hidden list
+//     $vlinks.children().last().prependTo($hlinks);
+
+//     // Show the dropdown btn
+//     if($btn.hasClass('hidden')) {
+//       $btn.removeClass('hidden');
+//     }
+
+//   // The visible list is not overflowing
+//   } else {
+
+//     // There is space for another item in the nav
+//     if(availableSpace > breaks[breaks.length-1]) {
+
+//       // Move the item to the visible list
+//       $hlinks.children().first().appendTo($vlinks);
+//       breaks.pop();
+//     }
+
+//     // Hide the dropdown btn if hidden list is empty
+//     if(breaks.length < 1) {
+//       $btn.addClass('hidden');
+//       $hlinks.addClass('hidden');
+//     }
+//   }
+
+//   // Keep counter updated
+//   $btn.attr("count", breaks.length);
+
+//   // Recur if the visible list is still overflowing the nav
+//   if($vlinks.width() > availableSpace) {
+//     updateNav();
+//   }
+
+// }
+
+// // Window listeners
+
+// $(window).resize(function() {
+//   updateNav();
+// });
+
+// $btn.on('click', function() {
+//   $hlinks.toggleClass('hidden');
+//   $(this).toggleClass('close');
+// });
+
+// updateNav();
+
+
+
